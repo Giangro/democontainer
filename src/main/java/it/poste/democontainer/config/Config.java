de62e53ec8086bf6442e4b9867802bd23a62e736
giangr40 <alessandro.giangreco@posteitaliane.it>
2021-10-22 19:32:38 +0200

    transactional producer

--- src/main/java/it/poste/democontainer/config/Config.java
+++ src/main/java/it/poste/democontainer/config/Config.java
@@ -6,16 +6,26 @@
 package it.poste.democontainer.config;
 
 import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.producer.ProducerInterceptor;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.TopicPartition;
 import org.springframework.cloud.stream.binder.kafka.KafkaBindingRebalanceListener;
 import org.springframework.cloud.stream.config.ListenerContainerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.kafka.listener.AbstractMessageListenerContainer;
 import org.springframework.kafka.listener.DefaultAfterRollbackProcessor;
+import org.springframework.messaging.support.ErrorMessage;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,6 +39,9 @@ import org.springframework.util.backoff.FixedBackOff;
 @EnableTransactionManagement
 @Slf4j
 public class Config {
+
+    private AtomicInteger messageCounter
+            = new AtomicInteger(0);
 
     @Bean
     KafkaBindingRebalanceListener kafkaBindingRebalanceListener() {
@@ -72,8 +85,57 @@ public class Config {
         @Transactional
         String run(String msg) {
             log.info("Received event={}", msg);
+            try {
+                Thread.sleep(6_000);
+            } catch (InterruptedException ex) {
+            }
             return msg.toUpperCase();
         }
+    }
+
+    @Bean
+    public Supplier<String> supplier() {
+        return () -> {
+            log.info("Sending new message");
+            return "Hello World! #" + messageCounter.incrementAndGet();
+        };
+    }
+
+    @ServiceActivator(inputChannel = "inboundtopic.errors")
+    public void errorProcessHandler(ErrorMessage em) {
+        log.error("@@@ errorProcessHandler {}", em.toString());
+    }
+
+    @ServiceActivator(inputChannel = "outboundtopic.errors")
+    public void errorSupplierHandler(ErrorMessage em) {
+        log.error("@@@ errorSupplierHandler {}", em.toString());
+    }
+
+    public static class DefaultProducerInterceptor implements ProducerInterceptor<byte[], byte[]> {
+
+        @Override
+        public void configure(Map<String, ?> configs) {
+        }
+
+        @Override
+        public ProducerRecord<byte[], byte[]> onSend(ProducerRecord<byte[], byte[]> record) {
+            return record;
+        }
+
+        @Override
+        public void onAcknowledgement(RecordMetadata metadata, Exception exception) {
+            if (metadata != null) {
+                System.out.println(metadata);
+            }
+            if (exception != null) {
+                System.out.println(exception);
+            }
+        }
+
+        @Override
+        public void close() {
+        }
+
     }
 
 }
